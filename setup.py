52e8d7791ace14ddb254b269843cc899deac4b2a
SopaXorzTaker <yellowmark@yandex.ru>
2015-07-07 15:03:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gamecheck',
+    version='',
+    packages=['server_ping', 'server_ping.mcpe', 'server_ping.minecraft'],
+    url='SopaXorzTaker.github.io',
+    license='',
+    author='Mark B.',
+    author_email='',
+    description='Py3/Flask game server checker'
+)
